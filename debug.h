3575412806765c22efb083d13ac98606a0e7b271
watercube <kostkawodna@gmail.com>
2025-10-05 21:52:28 +0200

    Update debug.h

--- debug.h
+++ debug.h
@@ -8,7 +8,6 @@ public:
     void DrawCursorPos(const Camera2D& camera);
     Rectangle CreateRectangleFromMouse(Vector2 start, Vector2 end);
 
-    // New function to handle rectangle drawing and printing
     void HandleDebugRectangle(const Camera2D& camera);
 
 private:
